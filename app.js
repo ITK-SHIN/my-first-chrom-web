7b38e83ad099e5fb367228e02f5bd83b9c35d40f
ITK-SHIN <105304446+ITK-SHIN@users.noreply.github.com>
2023-01-01 18:21:41 +0900

    login ~ 4.7 finish
    login finish

--- app.js
+++ app.js
@@ -3,14 +3,27 @@ const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
   event.preventDefault();
   loginForm.classList.toggle(HIDDEN_CLASSNAME);
   const username = loginInput.value;
-  localStorage.setItem("username", username);
+  localStorage.setItem(USERNAME_KEY, username);
+  paintGreetings();
+}
+
+function paintGreetings() {
+  const username = localStorage.getItem(USERNAME_KEY);
   greeting.innerText = `안녕하세요 ${username}님 환영합니다`;
   greeting.classList.toggle(HIDDEN_CLASSNAME);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+  loginForm.classList.toggle(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+  paintGreetings();
+}
